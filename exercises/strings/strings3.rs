90c0b3ca3d40557bd58639e83a41e84ad73af1bf
Max <keigu2008@outlook.com>
2022-11-03 02:59:36 +0800

    finish function `trim_me`,`compose_me` and `replace_me`

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,19 +3,21 @@
 
 // I AM NOT DONE
 
+use std::fmt::format;
+
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    input.trim().to_string()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    format!("{input} world!")
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    input.replace("cars", "balloons")
 }
 
 #[cfg(test)]
@@ -37,7 +39,13 @@ mod tests {
 
     #[test]
     fn replace_a_string() {
-        assert_eq!(replace_me("I think cars are cool"), "I think balloons are cool");
-        assert_eq!(replace_me("I love to look at cars"), "I love to look at balloons");
+        assert_eq!(
+            replace_me("I think cars are cool"),
+            "I think balloons are cool"
+        );
+        assert_eq!(
+            replace_me("I love to look at cars"),
+            "I love to look at balloons"
+        );
     }
 }
