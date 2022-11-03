2d00f1e0e8e97cd620353590e49869bd84601639
Max <keigu2008@outlook.com>
2022-11-03 22:52:27 +0800

    change `match y` to `match &y` for reusing variable y

--- exercises/options/options3.rs
+++ exercises/options/options3.rs
@@ -8,7 +8,6 @@ struct Point {
 
 fn main() {
     let y: Option<Point> = Some(Point { x: 100, y: 200 });
-
     match &y {
         Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
         _ => println!("no match"),
