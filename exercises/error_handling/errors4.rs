e6013af0caf895105c1d17e55b4cc00432f76c11
Max <keigu2008@outlook.com>
2022-11-03 23:51:09 +0800

    add `match` statement

--- exercises/error_handling/errors4.rs
+++ exercises/error_handling/errors4.rs
@@ -1,8 +1,6 @@
 // errors4.rs
 // Execute `rustlings hint errors4` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 #[derive(PartialEq, Debug)]
 struct PositiveNonzeroInteger(u64);
 
@@ -15,7 +13,11 @@ enum CreationError {
 impl PositiveNonzeroInteger {
     fn new(value: i64) -> Result<PositiveNonzeroInteger, CreationError> {
         // Hmm...? Why is this only returning an Ok value?
-        Ok(PositiveNonzeroInteger(value as u64))
+        match value {
+            eq_0 if eq_0 == 0 => Err(CreationError::Zero),
+            less_0 if less_0 < 0 => Err(CreationError::Negative),
+            _else => Ok(PositiveNonzeroInteger(value as u64)),
+        }
     }
 }
 
